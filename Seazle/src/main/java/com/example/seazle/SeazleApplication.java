68acf3f622dae92789cdb756c1bd6e127875e40f
LukeSeobyungju <22100356@handong.ac.kr>
2025-08-10 15:17:02 +0900

    init : 프로젝트생성

---
+++ Seazle/src/main/java/com/example/seazle/SeazleApplication.java
@@ -0,0 +1,13 @@
+package com.example.seazle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeazleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeazleApplication.class, args);
+    }
+
+}
